a969abfff433e5cfe851e302652c8187b4c716b4
krampus <ms-krasti@mail.ru>
2018-12-19 16:53:16 +0600

    lesson 65

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,29 @@
 $(document).ready(function() {
-  $('.owl-carousel').owlCarousel({
-    dots: false,
-    items: 1,
-    loop: true,
-    nav: true,
-    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+	$('.slider').owlCarousel({
+		dots: false,
+		items: 1,
+		loop: true,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
 })
+
+	$('.offers__list').owlCarousel({
+		// dots: false,
+		items: 6,
+		margin: 20
+		// loop: true,
+		// nav: false,
+		// navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+	})
+
+
+	$('.commit__list').owlCarousel({
+		// dots: false,
+		items: 4,
+		autoplay: true
+		// loop: true,
+		// nav: false,
+		// navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+	})
 
 });
